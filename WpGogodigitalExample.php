29ac84681ec439138fce59f3a268848f6fe7c62b
Giando <giando.olini@gogodigital.it>
2019-02-08 09:49:50 +0100

    Adding first files

---
+++ WpGogodigitalExample.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * Author: Gogodigital S.r.l.s.
+ * Author URI: https://www.gogodigital.it
+ **/
+
+if ( !defined( 'ABSPATH' ) ) {
+	header( 'Status: 403 Forbidden' );
+	header( 'HTTP/1.1 403 Forbidden' );
+	exit;
+}
+
+class WpGogodigitalExample
+{
+
+}
---
+++ gogodigital-example.php
@@ -0,0 +1,30 @@
+<?php
+
+/**
+ * Plugin Name: Gogodigital Example Plugin
+ * Description: Example Plugin for Wordpress
+ * Author: Gogodigital S.r.l.s.
+ * Author URI: https://www.gogodigital.it
+ * Version: 1.0.0
+ * Text Domain: gogodigital-example
+ **/
+
+if ( !defined( 'ABSPATH' ) ) {
+	header( 'Status: 403 Forbidden' );
+	header( 'HTTP/1.1 403 Forbidden' );
+	exit;
+}
+
+require_once 'WpGogodigitalExample.php';
+
+function gogodigital_example_register_settings()
+{
+	add_option( 'gogodigital-example-input');
+	register_setting( 'gogodigital_example_options_group', 'gogodigital-example-input', 'gogodigital_example_callback' );
+}
+
+add_action( 'admin_init', 'gogodigital_example_register_settings' );
+
+if( is_admin() ) {
+	$importer_page = new WpGogodigitalExample();
+}
