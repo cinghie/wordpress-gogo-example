3d57625140f4917a1fa8f0819074816c017c35c5
Giando <giando.olini@gogodigital.it>
2019-02-20 00:38:25 +0100

    Adding fields

--- WpGogodigitalExample.php
+++ WpGogodigitalExample.php
@@ -32,6 +32,12 @@ class WpGogodigitalExample
 	/** @var string */
 	private $inputExample;
 
+	/** @var string */
+	private $radioExample;
+
+	/** @var string */
+	private $selectExample;
+
 	/**
 	 * Class Constructor
 	 *
@@ -50,6 +56,8 @@ class WpGogodigitalExample
 
 		/** Set Settings values */
 		$this->inputExample = get_option('gogodigital-example-input');
+		$this->radioExample = get_option('gogodigital-example-radio');
+		$this->selectExample = get_option('gogodigital-example-select');
 
 		/** Register Settings */
 		add_action( 'admin_init', array($this,'gogodigital_example_register_settings') );
@@ -77,11 +85,23 @@ class WpGogodigitalExample
 	 */
     public function gogodigital_example_register_settings()
 	{
-		/** Add input option */
+		/** Add Example Input Option */
 		add_option( 'gogodigital-example-input');
 
-		/** Register input option */
+		/** Add Example Radio Option */
+		add_option( 'gogodigital-example-radio');
+
+		/** Add Example Select Option */
+		add_option( 'gogodigital-example-select');
+
+		/** Register Example Input Option */
 		register_setting( 'gogodigital_example_options_group', 'gogodigital-example-input', 'gogodigital_example_callback' );
+
+		/** Register Example Input Option */
+		register_setting( 'gogodigital_example_options_group', 'gogodigital-example-radio', 'gogodigital_example_callback' );
+
+		/** Register Example Select Optio */
+		register_setting( 'gogodigital_example_options_group', 'gogodigital-example-select', 'gogodigital_example_callback' );
 	}
 
 	/**
@@ -93,21 +113,53 @@ class WpGogodigitalExample
 
 		<div class="wrap" style="overflow: hidden;">
 
-			<div style="border-right: 1px solid #ddd; float: left; padding-right: 2%;  width: 100%">
+			<div>
                 <h1><?php echo $this->pageTitle ?></h1>
                 <p><?php echo __( 'Gogodigital Example plugin just a simple example to develop a new Wordpress Plugin', 'gogodigital-example' ) ?></p>
 			</div>
-			<br/><br/>
 
-			<form method="post" action="options.php">
+			<form class="form-table" method="post" action="options.php">
 				<?php settings_fields( 'gogodigital_example_options_group' ); ?>
 				<table class="form-table">
 					<tr valign="top">
-						<td >
-							<label style="margin-right: 25px;"><?php echo __( 'Example Input', 'gogodigital-example' ) ?></label>
-							<input class="form-control" style="width:250px; padding: 6px;" type="text" id="gogodigital-example-input" name="gogodigital-example-input"  value="<?php echo $this->inputExample ?>" >
+                        <th scope="row">
+                            <label for="gogodigital-example-input"><?php echo __( 'Example Input', 'gogodigital-example' ) ?></label>
+                        </th>
+						<td>
+							<input class="form-control" type="text" id="gogodigital-example-input" name="gogodigital-example-input" value="<?php echo $this->inputExample ?>" style="min-width: 250px; padding: 6px;">
 						</td>
 					</tr>
+                    <tr valign="top">
+                        <th scope="row">
+                            <label for="gogodigital-example-select"><?php echo __( 'Example Select', 'gogodigital-example' ) ?></label>
+                        </th>
+                        <td>
+                            <select class="form-control" id="gogodigital-example-select" name="gogodigital-example-select" style="min-width: 250px; padding: 3px;">
+                                <option value='1' <?php if ($this->selectExample === '1') { echo 'selected'; } else { echo ''; } ?>>Select Value 1</option>
+                                <option value='2' <?php if ($this->selectExample === '2') { echo 'selected'; } else { echo ''; } ?>>Select Value 2</option>
+                                <option value='3' <?php if ($this->selectExample === '3') { echo 'selected'; } else { echo ''; } ?>>Select Value 3</option>
+                            </select>
+                        </td>
+                    </tr>
+                    <tr valign="top">
+                        <th scope="row">
+                            <label for="gogodigital-example-select"><?php echo __( 'Example Radio', 'gogodigital-example' ) ?></label>
+                        </th>
+                        <td>
+                            <p>
+                                <input type="radio" id="gogodigital-example-radio"  name="gogodigital-example-radio" value="radiovalue1"<?php if ( $this->radioExample === 'radiovalue1' ) echo ' checked'; ?> />
+                                <label for="gogodigital-example-radiovalue1">Radio Value 1</label>
+                            </p>
+                            <p>
+                                <input type="radio" id="gogodigital-example-radio" name="gogodigital-example-radio" value="radiovalue2"<?php if ( $this->radioExample === 'radiovalue2' ) echo ' checked'; ?> />
+                                <label for="gogodigital-example-radiovalue2">Radio Value 2</label>
+                            </p>
+                            <p>
+                                <input type="radio" id="gogodigital-example-radio" name="ogodigital-example-radio" value="radiovalue3"<?php if ( $this->radioExample === 'radiovalue3' ) echo ' checked'; ?> />
+                                <label for="classic-editor-radiovalue2">Radio Value 3</label>
+                            </p>
+                        </td>
+                    </tr>
 				</table>
 				<div style="margin-top: 15px;">
 					<input type="submit" name="save" value="<?php echo __( 'Save Settings', 'gogodigital-example' ) ?>" class="button-primary" />
